6618bcf26555fb6264651ea788dd1be036c76471
chihieuvo43 <chihieuvo43@gmail.com>
2018-09-30 22:13:32 +0700

    the first commit

---
+++ tedushop.model/Abstract/IAuditable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tedushop.model.model
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string UpdatedBy { get; set; }
+
+        string MetaKeyword { get; set; }
+        string MetaDiscription { get; set; }
+
+        bool Status { get; set; }
+    }
+}
